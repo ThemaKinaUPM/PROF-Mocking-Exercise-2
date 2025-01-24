b4733e65ceed0868be86504c036ab8f4133339d5
ThemaKinaUPM <123257981+ThemaKinaUPM@users.noreply.github.com>
2025-01-24 15:54:26 +0000

    Corrected test
    Fixed missing import

--- src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
+++ src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
@@ -1,6 +1,7 @@
 package es.grise.upm.profundizacion.mocking.exercise2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doCallRealMethod;
